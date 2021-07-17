e8dffe840b9095a7e036bdf07dd13ea6dd92b487
satvikchaudhary-msit <83381004+satvikchaudhary-msit@users.noreply.github.com>
2021-07-17 23:28:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ screen = Screen()
 
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("My Snake Game")
+screen.title("Satvik's Snake Game")
 screen.tracer(0)
 
 snake = Snake()
